05f31b1134188b79a564753efdcd404796b8dff2
Scott DePouw <scott.depouw@gmail.com>
2025-02-10 07:15:26 -0500

    Created HUD and wired everything together

--- Dodge the Creeps/Main/main.gd
+++ Dodge the Creeps/Main/main.gd
@@ -3,27 +3,29 @@ extends Node
 @export var mob_scene: PackedScene
 var score: int
 
-func _ready() -> void:
-  pass
-
 func game_over() -> void:
   $ScoreTimer.stop()
   $MobTimer.stop()
+  $HUD.show_game_over()
 
 func new_game() -> void:
   score = 0
   $Player.start($StartPosition.position)
   $StartTimer.start()
+  $HUD.update_score(score)
+  $HUD.show_message("Get Ready")
+  get_tree().call_group("mobs", "queue_free")
 
 func _on_score_timer_timeout() -> void:
   score += 1
+  $HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
   $MobTimer.start()
   $ScoreTimer.start()
 
 func _on_mob_timer_timeout() -> void:
-  # Create a new nstance of the Mob scene.
+  # Create a new instance of the Mob scene.
   var mob: RigidBody2D = mob_scene.instantiate() as RigidBody2D
   assert(mob is RigidBody2D and mob != null, "Mob instance must be of type RigidBody2D")
 
