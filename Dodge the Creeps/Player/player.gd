c94b1b39b829ce49c42b1633dc89fbc79341aa48
Scott DePouw <scott.depouw@gmail.com>
2025-02-10 12:37:20 -0500

    Added iframes to Player with animation
    Blinks when in iframes, and cannot take damage while blinking

--- Dodge the Creeps/Player/player.gd
+++ Dodge the Creeps/Player/player.gd
@@ -1,14 +1,19 @@
 extends Area2D
 
 var player_is_dead: bool = false
+var player_is_invincible: bool = false
 var current_health: int
 var screen_size: Vector2
 
 signal hit(old_health: int, new_health: int)
 signal death
 
+## Starting health for Player
 @export var starting_health: int = 3
+## Speed Player travels
 @export var speed: int = 400
+## Length of time (in seconds) Player is invulnerable after taking damage
+@export var iframe_time: float = 1.0
 
 func start(startPosition: Vector2) -> void:
   position = startPosition
@@ -20,13 +25,13 @@ func start(startPosition: Vector2) -> void:
 func _process(delta: float) -> void:
   move_player(delta)
   if get_overlapping_bodies().any(is_mob_body):
-    take_damage()
+    try_take_damage()
 
 func is_mob_body(body: Node2D) -> bool:
   return body.is_in_group("mobs")
 
-func take_damage(damage: int = 1) -> void:
-  if player_is_dead:
+func try_take_damage(damage: int = 1) -> void:
+  if player_is_invincible or player_is_dead:
     return
   var old_health: int = current_health
   current_health -= damage
@@ -35,6 +40,10 @@ func take_damage(damage: int = 1) -> void:
   hit.emit(old_health, current_health)
   if current_health == 0:
     death.emit()
+  else:
+    player_is_invincible = true
+    $TakingDamageAnimation.play("take_damage")
+    $AnimationTimer.start()
 
 func move_player(delta: float) -> void:
   var velocity: Vector2 = Vector2.ZERO # The player's movement vector.
@@ -73,4 +82,9 @@ func _on_death() -> void:
 
 func _ready() -> void:
   screen_size = get_viewport_rect().size
+  $AnimationTimer.wait_time = iframe_time
   hide()
+
+func _on_animation_timer_timeout() -> void:
+  player_is_invincible = false
+  $TakingDamageAnimation.play("RESET")
