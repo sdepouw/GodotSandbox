163420aaa62ab1d65d0c7d1cb23fa7f70976557a
Scott DePouw <scott.depouw@gmail.com>
2025-02-14 13:59:48 -0500

    Add Player Code
    First time using C# in Godot! Enabled nullability, warnings as errors, and did a little C# magic.

---
+++ Dodge the Creeps CSharp/InputAction.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace DodgeTheCreeps;
+
+public sealed class InputAction
+{
+  public static readonly InputAction Right = new("move_right");
+  public static readonly InputAction Left = new("move_left");
+  public static readonly InputAction Up = new("move_up");
+  public static readonly InputAction Down = new("move_down");
+
+  private readonly StringName _action;
+  private InputAction(string actionName) => _action = new(actionName);
+
+  public bool IsActionPressed() => Input.IsActionPressed(_action);
+}
