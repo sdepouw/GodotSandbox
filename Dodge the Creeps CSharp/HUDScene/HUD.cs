9e4d84d1f85599cbf7b07d1d60e121793a6b1de4
Scott DePouw <scott.depouw@gmail.com>
2025-02-14 19:22:51 -0500

    Created the HUD scene

---
+++ Dodge the Creeps CSharp/HUDScene/HUD.cs
@@ -0,0 +1,51 @@
+using System.Threading.Tasks;
+using Godot;
+
+namespace DodgeTheCreeps.HUDScene;
+
+public partial class HUD : CanvasLayer
+{
+  [Signal] public delegate void StartGameEventHandler();
+
+  private string _defaultMessageText = "";
+  
+  public override void _Ready() => _defaultMessageText = GetNode<Label>("MessageText").Text;
+
+  public override void _Process(double delta)
+  {
+  }
+
+  private void ShowMessage(string text)
+  {
+    Label message = GetNode<Label>("Message");
+    message.Text = text;
+    message.Show();
+    
+    GetNode<Timer>("MessageTimer").Start();
+  }
+
+  private async Task ShowGameOver()
+  {
+    ShowMessage("Game Over");
+    
+    Timer messageTimer = GetNode<Timer>("MessageTimer");
+    await ToSignal(messageTimer, Timer.SignalName.Timeout);
+    
+    Label message = GetNode<Label>("Message");
+    message.Text = _defaultMessageText;
+    message.Show();
+    
+    await ToSignal(GetTree().CreateTimer(1.0), SceneTreeTimer.SignalName.Timeout);
+    GetNode<Button>("StartButton").Show();
+  }
+  
+  private void UpdateScore(int score) => GetNode<Label>("Score").Text = score.ToString();
+
+  private void OnStartButtonPressed()
+  {
+    GetNode<Button>("StartButton").Hide();
+    EmitSignal(SignalName.StartGame);
+  }
+  
+  private void OnMessageTimerTimeout() => GetNode<Label>("Message").Hide();
+}
