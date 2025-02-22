9ef4d2d3871d7604e2cdab94759b4fec49df00a0
Scott DePouw <scott.depouw@gmail.com>
2025-02-22 14:23:02 -0500

    Completed tutorial

--- First 2D Game From Zero/Game/player.gd
+++ First 2D Game From Zero/Game/player.gd
@@ -1,8 +1,25 @@
 extends CharacterBody2D
 
-@onready var _happy_boo: Node2D = $HappyBoo
+signal health_depleted
 
-func _physics_process(_delta: float) -> void:
+## Current health of the Player
+@export var health: float = 150.0:
+  set(value):
+    health = value
+    _health_bar.value = value
+## How much damage enemies do when they collide with the Player
+@export var damage_rate: float = 5.0
+
+@onready var _happy_boo: Node2D = $HappyBoo
+@onready var _hurt_box: Area2D = %HurtBox
+@onready var _health_bar: ProgressBar = $HealthBar
+
+func _ready() -> void:
+  _health_bar.max_value = health
+  _health_bar.value = health
+
+# Delta is applied automatically by move_and_slide()
+func _physics_process(delta: float) -> void:
   var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
   velocity = direction * 600
   move_and_slide()
@@ -10,3 +27,9 @@ func _physics_process(_delta: float) -> void:
     _happy_boo.play_idle_animation()
   else:
     _happy_boo.play_walk_animation()
+
+  var overlapping_mobs = _hurt_box.get_overlapping_bodies()
+  if overlapping_mobs.size() > 0:
+    health -= damage_rate * overlapping_mobs.size() * delta
+    if health <= 0.0:
+      health_depleted.emit()
